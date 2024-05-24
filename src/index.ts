875fa51e6295f7ca1c677941ae147496707a9db8
TheGod7 <br.iancefi12@gmail.com>
2024-05-23 23:40:38 -0600

    host Update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { BotAI } from "./client.js";
+import express from "express";
 
+const app = express();
 async function Start() {
   const client = new BotAI();
 
   await client.init();
+
+  app.listen(3000, () => {
+    console.log("a");
+  });
+
+  app.get("/", (req, res) => {
+    return res.send("a");
+  });
 }
 
 Start();
